7f1e6c3623460a79153e209462c4f42e3c8acac7
jm-go <jowita.m.gogol@gmail.com>
2025-03-02 17:23:05 +0000

    feat(ui): Added TensorFlowProvider to app context

---
+++ frontend/hooks/TensorFlowProvider.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
+import { bundleResourceIO } from '@tensorflow/tfjs-react-native';
+import { View, ActivityIndicator } from 'react-native';
+import { Colors } from '@/constants/Colors';
+
+const TensorFlowContext = createContext<{ isModelReady: boolean; model: tf.LayersModel | null }>({
+  isModelReady: false,
+  model: null,
+});
+
+export const useTensorFlow = () => useContext(TensorFlowContext);
+
+export const TensorFlowProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isModelReady, setIsModelReady] = useState(false);
+  const [model, setModel] = useState<tf.LayersModel | null>(null);
+
+  useEffect(() => {
+    const loadModel = async () => {
+      try {
+        await tf.ready();
+        console.log('TensorFlow is ready');
+
+        const modelJson = require('../assets/model/model.json');
+        const modelWeights = [
+          require('../assets/model/group1-shard1of3.bin'),
+          require('../assets/model/group1-shard2of3.bin'),
+          require('../assets/model/group1-shard3of3.bin'),
+        ];
+
+        const loadedModel = await tf.loadLayersModel(bundleResourceIO(modelJson, modelWeights));
+        console.log('Model loaded successfully');
+        setModel(loadedModel);
+        setIsModelReady(true);
+      } catch (error) {
+        console.error('Error loading TensorFlow model:', error);
+      }
+    };
+
+    loadModel();
+  }, []);
+
+  if (!isModelReady) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color={Colors.dark.darkPurple} />
+      </View>
+    );
+  }
+
+  return (
+    <TensorFlowContext.Provider value={{ isModelReady, model }}>
+      {children}
+    </TensorFlowContext.Provider>
+  );
+};
